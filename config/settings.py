a0061c316a00e23624cd247b21d067f3566a65bb
Dogmin <kdm3578@gmail.com>
2021-11-28 22:35:42 +0900

    CBV(#1.4)

--- config/settings.py
+++ config/settings.py
@@ -140,3 +140,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Auth
 
 AUTH_USER_MODEL = "users.User"
+
+
+# Django Rest Framework
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,11 @@
-from rest_framework.decorators import api_view
+from django.db.models import query
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from rest_framework.serializers import Serializer
 from .models import Room
 from .serializers import RoomSerializer
 
 
-@api_view(["GET", "DELETE"])
-def list_rooms(request):
-    rooms = Room.objects.all()
-    serialized_rooms = RoomSerializer(rooms, many=True)
-    return Response(data=serialized_rooms.data)
+class ListRoomsView(ListAPIView):
+
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
